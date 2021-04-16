5344d01dfbd6b75fb0859736e56bba1c95f52d62
thanhdatt31 <thanhdat2k14@gmail.com>
2021-04-16 11:03:51 +0700

    update support multi screen

--- app/src/main/java/com/example/kotlin03/CardActivity.kt
+++ app/src/main/java/com/example/kotlin03/CardActivity.kt
@@ -2,10 +2,14 @@ package com.example.kotlin03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_card.*
 
 class CardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card)
+        ic_back.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/kotlin03/PayActivity.kt
+++ app/src/main/java/com/example/kotlin03/PayActivity.kt
@@ -2,10 +2,15 @@ package com.example.kotlin03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_pay.*
+
 
 class PayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pay)
+        ic_back.setOnClickListener {
+            finish()
+        }
     }
 }
